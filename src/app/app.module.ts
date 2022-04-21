4e02b45237c635a97ba25bf56c9596d14d873434
xiller <aleks.kalinichenko989@gmail.com>
2022-04-21 19:15:55 +0300

    feat: Add base home skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/header/header.component';
+import { AppBarComponent } from './home/app-bar/app-bar.component';
+import { PreviewCardComponent } from './home/preview-card/preview-card.component';
+import { CardAreaComponent } from './home/card-area/card-area.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AppBarComponent,
+    PreviewCardComponent,
+    CardAreaComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
